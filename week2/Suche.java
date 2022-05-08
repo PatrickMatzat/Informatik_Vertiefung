9617e1f7eb1d230f0d6475d765ec18935f67af34
armando.flores <armando.flores@endava.com>
2022-05-08 23:04:27 +0200

    final working HA version

--- week2/Suche.java
+++ week2/Suche.java
@@ -1,5 +1,21 @@
 public class Suche  {    
     // Suchmethode
-    // TODO
+    public static int binaereSuche(Film suchObjekt, Film[] array){
+        int max = array.length -1;
+        int min = 0;
+        int index, diff;
+        while(min <= max){
+            index = min + (max - min)/2;
+            diff = suchObjekt.compareTo(array[index]);
+            if(diff == 0){
+                return index;
+            }else if(diff > 0){
+                min = index + 1;
+            }else{
+                max = index - 1;
+            }
+        }
+        return -1;
+    }
 
 }
